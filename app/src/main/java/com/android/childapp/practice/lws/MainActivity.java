b4b220b1f7be088ed28157372f898a63e763afba
Lucky <lucky80@gmail.com>
2017-05-06 00:53:46 +0900

    이원석 첫 Commit

--- app/src/main/java/com/android/childapp/practice/lws/MainActivity.java
+++ app/src/main/java/com/android/childapp/practice/lws/MainActivity.java
@@ -2,6 +2,8 @@ package com.android.childapp.practice.lws;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.android.childapp.R;
 
@@ -12,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice_lws_main);
 
+    }
+
+    public void onButton1Clicked(View v) {
+        Toast.makeText(getApplicationContext(), "사랑해요 :)", Toast.LENGTH_LONG).show();
     }
 }
