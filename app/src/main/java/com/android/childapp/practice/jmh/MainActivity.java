72be0a1954ab886ea850ba546d6560460e084054
MyungHeeJun <s981279@gmail.com>
2017-05-22 01:02:21 +0900

    [2017.05.22] 버튼,텍스트박스 생성

--- app/src/main/java/com/android/childapp/practice/jmh/MainActivity.java
+++ app/src/main/java/com/android/childapp/practice/jmh/MainActivity.java
@@ -2,15 +2,22 @@ package com.android.childapp.practice.jmh;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.android.childapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txtResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice_jmh_main);
+    }
 
+    public void onClickBtnEvent(View v) {
+        txtResult.setText("Good Job!");
     }
 }
