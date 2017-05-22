0ac5ab9f1bd8a31f598e80c40b646de60d817453
TaeEunKim <amiablete@naver.com>
2017-05-22 13:31:01 +0900

    김태은 개인과제

--- app/src/main/java/com/android/childapp/practice/kte/MainActivity.java
+++ app/src/main/java/com/android/childapp/practice/kte/MainActivity.java
@@ -1,16 +1,78 @@
 package com.android.childapp.practice.kte;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import java.util.List;
 
 import com.android.childapp.R;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice_kte_main);
 
+        Button tebtn = (Button) findViewById(R.id.tebtn);
+        Button tebtn2 = (Button) findViewById(R.id.tebtn2);
+
+        tebtn.setOnClickListener(new View.OnClickListener()
+                                 {
+                                     public void onClick(View v)
+                                     {
+                                         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&oquery=rgb+%EC%83%89%EC%83%81%ED%91%9C&ie=utf8&query=%EC%98%A4%EB%B2%84%EC%9B%8C%EC%B9%98"));
+                                         startActivity(intent);
+                                     }
+                                 }
+        );
+
+        tebtn2.setOnClickListener(new View.OnClickListener()
+                                 {
+                                     public void onClick(View v)
+                                     {
+                                         if(getPackageList()==true) {
+                                             Intent intent = getPackageManager().getLaunchIntentForPackage("com.dveamer.owfriends");
+                                             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                             startActivity(intent);
+                                         }else{
+                                             String url = "market://details?id=" + "com.dveamer.owfriends";
+                                             Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                                             startActivity(i);
+                                         }
+                                     }
+                                 }
+        );
     }
+
+    public boolean getPackageList() {
+        boolean isExist = false;
+
+        PackageManager pkgMgr = getPackageManager();
+        List<ResolveInfo> mApps;
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        mApps = pkgMgr.queryIntentActivities(mainIntent, 0);
+
+        try {
+            for (int i = 0; i < mApps.size(); i++) {
+                if(mApps.get(i).activityInfo.packageName.startsWith("com.dveamer.owfriends")){
+                    isExist = true;
+                    break;
+                }
+            }
+        }
+        catch (Exception e) {
+            isExist = false;
+        }
+        return isExist;
+    }
+
 }
