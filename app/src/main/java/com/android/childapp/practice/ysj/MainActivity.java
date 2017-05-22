54096fc991602ebb9f925c5380f00daae44bb38a
sjyoon914 <hasehaseoverwatch@gmail.com>
2017-05-22 13:07:20 +0900

    개인과제

--- app/src/main/java/com/android/childapp/practice/ysj/MainActivity.java
+++ app/src/main/java/com/android/childapp/practice/ysj/MainActivity.java
@@ -1,16 +1,91 @@
 package com.android.childapp.practice.ysj;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.android.childapp.R;
+import com.android.childapp.practice.ysj.database.DatabaseOpenHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SQLiteDatabase db = null;
+
+    private Button mInsertBtn;
+    private Button mSelectBtn;
+    private Button mDeleteAllBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice_ysj_main);
 
+        mInsertBtn = (Button) findViewById(R.id.ysj_insert);
+        mSelectBtn = (Button) findViewById(R.id.ysj_select);
+        mDeleteAllBtn = (Button) findViewById(R.id.ysj_deleteAll);
+
+        mInsertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createData();
+                Toast.makeText(MainActivity.this, "성공", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mSelectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String selectData = selectData();
+                Toast.makeText(MainActivity.this, selectData, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mDeleteAllBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteAllData();
+                Toast.makeText(MainActivity.this, "성공", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
+    private void createData() {
+        db = DatabaseOpenHelper.getInstance(this).getWritableDatabase();
+        String sql = "insert into TB_TEMP (SEQNO, TEMP1) values ('seq1', '1111')";
+        db.execSQL(sql);
+    }
+
+    private String selectData() {
+        db = DatabaseOpenHelper.getInstance(this).getWritableDatabase();
+        String sql = "select TEMP1 FROM TB_TEMP";
+        Cursor cursor = db.rawQuery(sql, null);
+
+        StringBuffer sb = new StringBuffer();
+
+        if(cursor.getCount() > 0) {
+            cursor.moveToFirst();
+            while(!cursor.isAfterLast()) {
+                String temp = cursor.getString(cursor.getColumnIndex("TEMP1"));
+                sb.append(temp);
+                sb.append("\n");
+                cursor.moveToNext();
+            }
+        }
+        cursor.close();
+
+        return sb.toString();
+    }
+
+    private void deleteAllData() {
+        db = DatabaseOpenHelper.getInstance(this).getWritableDatabase();
+        String sql = "delete from TB_TEMP";
+        db.execSQL(sql);
+    }
+
+
 }
---
+++ app/src/main/java/com/android/childapp/practice/ysj/database/DatabaseOpenHelper.java
@@ -0,0 +1,46 @@
+package com.android.childapp.practice.ysj.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by sjyoon on 2017-05-22.
+ */
+
+public class DatabaseOpenHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "ysj.db";
+    private static final int DB_VERSION = 1;
+
+    private static DatabaseOpenHelper _instance = null;
+
+    public DatabaseOpenHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    public synchronized static DatabaseOpenHelper getInstance(Context context){
+        if(_instance == null){
+            _instance = new DatabaseOpenHelper(context.getApplicationContext());
+        }
+        return _instance;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d("ysj", "onCreate");
+        String sql = "CREATE TABLE TB_TEMP (SEQNO TEXT, TEMP1 TEXT, TEMP2 TEXT, TEMP3 TEXT)";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.d("ysj", "onUpgrade oldVersion: " + oldVersion + ", newVersion: " + newVersion);
+        if(newVersion == 2) {
+            String sql = "ALTER TABLE TB_TEMP ADD COLUMN TEMP4 TEXT;";
+            db.execSQL(sql);
+            Log.d("ysj", "onUpgrade complete");
+        }
+    }
+}
