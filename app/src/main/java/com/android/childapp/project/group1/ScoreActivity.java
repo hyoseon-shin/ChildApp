e34f8ddb49b4d093ce35e929fcbe4e82caba6e6c
sjyoon914 <hasehaseoverwatch@gmail.com>
2017-06-12 09:57:02 +0900

    Score Activity 추가

---
+++ app/src/main/java/com/android/childapp/project/group1/ScoreActivity.java
@@ -0,0 +1,20 @@
+package com.android.childapp.project.group1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.childapp.R;
+
+/**
+ * Created by sjyoon on 2017-06-12.
+ */
+
+public class ScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_group1_score);
+
+    }
+}
