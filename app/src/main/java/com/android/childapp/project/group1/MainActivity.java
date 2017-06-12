f9171fa9c0de488f3e51d0eaf4f9ad7cf53fbb2b
sjyoon914 <hasehaseoverwatch@gmail.com>
2017-06-12 10:06:25 +0900

    Score Activity 호출용 임시

--- app/src/main/java/com/android/childapp/project/group1/MainActivity.java
+++ app/src/main/java/com/android/childapp/project/group1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.android.childapp.project.group1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.android.childapp.R;
 
@@ -11,10 +14,31 @@ import com.android.childapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mBtn1;
+    private Button mBtn2;
+    private Button mBtn3;
+    private Button mBtn4;
+    private Button mBtn5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_group1_main);
 
+        mBtn1 = (Button) findViewById(R.id.project_group1_btn1);
+        mBtn2 = (Button) findViewById(R.id.project_group1_btn2);
+        mBtn3 = (Button) findViewById(R.id.project_group1_btn3);
+        mBtn4 = (Button) findViewById(R.id.project_group1_btn4);
+        mBtn5 = (Button) findViewById(R.id.project_group1_btn5);
+
+        mBtn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, com.android.childapp.project.group1.ScoreActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
 }
