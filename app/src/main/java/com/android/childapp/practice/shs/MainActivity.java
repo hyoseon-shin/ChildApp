f2da878c7b799be6232074e2c6f0a02b35ee9848
hyoseon.shin <cctech00@gmail.com>
2017-06-16 00:54:12 +0900

    Commit for shs

--- app/src/main/java/com/android/childapp/practice/shs/MainActivity.java
+++ app/src/main/java/com/android/childapp/practice/shs/MainActivity.java
@@ -2,23 +2,25 @@ package com.android.childapp.practice.shs;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
+import android.util.SparseIntArray;
+import android.view.Display;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.android.childapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String CORRECT_ANSWER = "3";
+    private static final int MAX_HEIGHT = 460;
+    private static final String CORRECT_ANSWER = "3";
+
+    private static final String MSG_CORRECT = "정답입니다.";
+    private static final String MSG_WRONG = "오답입니다.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,73 +30,69 @@ public class MainActivity extends AppCompatActivity {
         TextView example = (TextView) findViewById(R.id.example);
         example.setText(CORRECT_ANSWER);
 
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int screenWidth = size.x;
+
         LinearLayout imageLayout = (LinearLayout) findViewById(R.id.image_layout);
+        imageLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        imageLayout.setOrientation(LinearLayout.VERTICAL);
 
-        LinearLayout imageRow1 = new LinearLayout(this);
-        imageRow1.setOrientation(LinearLayout.HORIZONTAL);
+        LinearLayout imageRow = new LinearLayout(this);
+        imageRow.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        imageRow.setOrientation(LinearLayout.HORIZONTAL);
 
-        ImageView image1 = new ImageView(this);
-        image1.setAdjustViewBounds(true);
-        image1.setMaxHeight(500);
-        image1.setImageResource(R.drawable.animal1_1);
-        image1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showAlert("1");
-            }
-        });
-        imageRow1.addView(image1);
+        double left = 0;
+        SparseIntArray imageResArr = getImageResList();
+        for(int i = 1; i <= imageResArr.size(); i++) {
+            int resId = imageResArr.get(i);
+            ImageView image = new ImageView(this);
+            image.setTag(i);
+            image.setAdjustViewBounds(true);
+            image.setMaxHeight(MAX_HEIGHT);
+            image.setImageResource(resId);
+            image.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    showAlert(String.valueOf(v.getTag()));
+                }
+            });
+            int imageWidth = image.getDrawable().getIntrinsicWidth();
+            int imageHeight = image.getDrawable().getIntrinsicHeight();
+            double resizeWidth = imageWidth * MAX_HEIGHT / imageHeight;
+            System.out.println("************ resizeWidth: " + resizeWidth);
+            imageLayout.addView(image);
+//            if (left + resizeWidth > screenWidth) {
+//                imageLayout.addView(imageRow);
+//                imageRow = new LinearLayout(this);
+//                imageRow.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+//                imageRow.setOrientation(LinearLayout.HORIZONTAL);
+//                imageRow.addView(image);
+//                left = resizeWidth;
+//            } else {
+//                imageRow.addView(image);
+//                left += resizeWidth;
+//            }
+        }
+    }
 
-        ImageView image2 = new ImageView(this);
-        image2.setAdjustViewBounds(true);
-        image2.setMaxHeight(500);
-        image2.setImageResource(R.drawable.animal2_1);
-        image2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showAlert("2");
-            }
-        });
-        imageRow1.addView(image2);
-
-        imageLayout.addView(imageRow1);
-
-        LinearLayout imageRow2 = new LinearLayout(this);
-        imageRow1.setOrientation(LinearLayout.HORIZONTAL);
-
-        ImageView image3 = new ImageView(this);
-        image3.setAdjustViewBounds(true);
-        image3.setMaxHeight(500);
-        image3.setImageResource(R.drawable.animal3_1);
-        image3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showAlert("3");
-            }
-        });
-        imageRow2.addView(image3);
-
-        ImageView image4 = new ImageView(this);
-        image4.setAdjustViewBounds(true);
-        image4.setMaxHeight(500);
-        image4.setImageResource(R.drawable.animal4_1);
-        image4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showAlert("4");
-            }
-        });
-        imageRow2.addView(image4);
-
-        imageLayout.addView(imageRow2);
+    // TODO: Retrieve resource data from DB
+    public SparseIntArray getImageResList() {
+        SparseIntArray rsltArr = new SparseIntArray();
+        rsltArr.put(1, R.drawable.animal1_1);
+        rsltArr.put(2, R.drawable.animal2_1);
+        rsltArr.put(3, R.drawable.animal3_1);
+        rsltArr.put(4, R.drawable.animal4_1);
+        return rsltArr;
     }
 
     public void showAlert(final String val) {
         String message;
         if (CORRECT_ANSWER.equals(val)) {
-            message = "정답입니다.";
+            message = MSG_CORRECT;
         } else {
-            message = "오답입니다.";
+            message = MSG_WRONG;
         }
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
